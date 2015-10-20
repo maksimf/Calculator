9330337759a633d6b66bf691fafbad2648162f27
Maksim Filippov <m.filippov@rambler-co.ru>
2015-10-20 18:53:25 +0300

    Semi-working π

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -16,37 +16,51 @@ class ViewController: UIViewController
 
     @IBAction func appendDigit(sender: UIButton) {
         let digit = sender.currentTitle!
+        let value = (digit == "π") ? "\(M_PI)" : digit
+
         if userIsInTheMiddleOfTypingANumber {
-            display.text = display.text! + digit
+            guard digit != "." || display.text!.rangeOfString(".") == nil else { return }
+            if digit == "π" {
+                enter()
+                display.text = value
+                enter()
+            } else {
+                display.text = display.text! + value
+            }
         } else {
-            display.text = digit
+            display.text = value
             userIsInTheMiddleOfTypingANumber = true
         }
     }
     
     @IBAction func operate(sender: UIButton) {
         let operation = sender.currentTitle!
+        
         if userIsInTheMiddleOfTypingANumber {
             enter()
         }
+        
         switch operation {
-        case "×": calcPerformOperation { $0 * $1 }
-        case "÷": calcPerformOperation { $1 / $0 }
-        case "+": calcPerformOperation { $0 + $1 }
-        case "−": calcPerformOperation { $1 - $0 }
-        case "√": calcPerformOperation { sqrt($0) }
+        case "×": performOperation { $0 * $1 }
+        case "÷": performOperation { $1 / $0 }
+        case "+": performOperation { $0 + $1 }
+        case "−": performOperation { $1 - $0 }
+        case "√": performOperation { sqrt($0) }
+        case "sin": performOperation { sin($0) }
+        case "cos": performOperation { cos($0) }
+        case "π": performOperation { M_PI * $0 }
         default: break
         }
     }
     
-    func calcPerformOperation(operation: (Double, Double) -> Double) {
+    func performOperation(operation: (Double, Double) -> Double) {
         if operandStack.count >= 2 {
             displayValue = operation(operandStack.removeLast(), operandStack.removeLast())
             enter()
         }
     }
     
-    private func calcPerformOperation(operation: Double -> Double) {
+    private func performOperation(operation: Double -> Double) {
         if operandStack.count >= 1 {
             displayValue = operation(operandStack.removeLast())
             enter()
